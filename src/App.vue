8a3407f992b9ee3fe6bda7c413dae73c14189402
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-05-03 15:05:54 +0200

    Milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Projects</h1>
+    <div class="project-list">
+      <project-card v-for="project in projects" :key="project.id" :project="project"></project-card>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import axios from 'axios'
+import ProjectCard from './components/ProjectCard.vue'
+
+export default {
+  components: {
+    ProjectCard
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  created() {
+    axios.get('/api/projects')
+      .then(response => {
+        this.projects = response.data
+        console.log(this.projects)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="project-card">
+        <h2>{{ project.titolo }}</h2>
+        <p>{{ project.descrizione }}</p>
+        <p><strong>Tecnologie:</strong> {{ project.tecnologie }}</p>
+        <a :href="project.url" target="_blank">mostra progetto</a>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+  
